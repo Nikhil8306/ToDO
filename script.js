51489bdd77119bb5d61719243ee07040ac0ef53a
Nikhil Kumar <nikhileng83@gmail.com>
2024-01-05 10:28:41 +0530

    fixing the wrong revert problem :|

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const toggleBtn = document.querySelector('.side-toggle');
 const sidebar = document.querySelector('.side-panel');
+const wrapper = document.querySelector('#wrapper');
+
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('sidebar-open');
 });
 
-function reportWindowSize() {
-    let height = window.innerHeight;
-    const wrapper = document.querySelector('#wrapper');
-    wrapper.style.height = height+'px';
-}
-reportWindowSize();
-window.onresize = reportWindowSize;
+// function reportWindowSize() {
+//     let height = window.innerHeight;
+//     wrapper.style.height = 100+'px';
+// }
+
+// reportWindowSize();
+// window.onresize = reportWindowSize;
