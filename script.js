45c76c9663188d350178a89042e124f3de6e80e8
Nikhil Kumar <nikhileng83@gmail.com>
2024-01-05 10:06:12 +0530

    tryping to solve mobile responsiveness: take 03

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const sidebar = document.querySelector('.side-panel');
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('sidebar-open');
 });
+
+function reportWindowSize() {
+    let height = window.innerHeight;
+    const wrapper = document.querySelector('#wrapper');
+    wrapper.style.height = height+'px';
+}
+reportWindowSize();
+window.onresize = reportWindowSize;
