fa6291dd82be087e940e28d8b1ea039be0c31c50
Nikhil Kumar <nikhileng83@gmail.com>
2024-01-06 20:59:51 +0530

    Few modifications on looks

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const toggleBtn = document.querySelector('.side-toggle');
 const sidebar = document.querySelector('.side-panel');
 const wrapper = document.querySelector('#wrapper');
 
+const homePage = document.querySelector('.home-content');
+const statPage = document.querySelector('.statistics-content');
+const taskPage = document.querySelector('.task-content');
+const comPage = document.querySelector('.community-content');
+
+const buttons = document.querySelectorAll('.panel-item');
+
+const panelItems = [homePage, statPage, comPage, taskPage];
+
+for(let i = 0; i < 4; i++){
+    buttons[i].addEventListener("click", function(){
+        for(let j = 0; j < 4; j++){
+            if (!panelItems[j].classList.contains('hide')) panelItems[j].classList.toggle('hide');
+        }
+        panelItems[i].classList.toggle('hide');
+        console.log("Hello");
+    })
+}
+
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('sidebar-open');
 });
